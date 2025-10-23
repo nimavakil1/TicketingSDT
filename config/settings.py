33bcb516d8ee8d8f2d5672dea8f789d691dc7e62
Nima Vakil <nimavakil@nimas-mbp.home>
2025-10-23 08:03:06 +0200

    Fix Settings validation to ignore extra env variables

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,8 @@ class Settings(BaseSettings):
         env_file='.env',
         env_file_encoding='utf-8',
         case_sensitive=False,
-        validate_default=True
+        validate_default=True,
+        extra='ignore'
     )
 
     # Ticketing API Configuration
